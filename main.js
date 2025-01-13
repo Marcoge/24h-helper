47f3a7241c058f03ce45af45607f44b5829c56ab
Marco Gester <m.gester@gmx.net>
2025-01-13 15:50:31 +0100

    Initial project setup.

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+
+const env = process.env.NODE_ENV || "development";
+
+function createWindow() {
+  const window = new BrowserWindow({
+    width: 1280,
+    height: 768,
+    show: false,
+    webPreferences: { 
+        contextIsolation: true,
+        preload: `${__dirname}/preload.js` },
+  });
+
+  window.webContents.on("did-finish-load", () => {
+    window.show();
+    window.focus();
+  });
+
+  window.loadFile("dist/24h-helper/browser/index.html");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
